ca5c55bff1d5cb7c714db71c12e21a289bc57dec
William Tu <wj654cj86@gmail.com>
2019-02-14 18:51:51 +0800

    V1.5.0 x OFC408L8 x feature_redisDevice 上傳新檔

---
+++ webchktool/html/simulator/sensor.js
@@ -0,0 +1,56 @@
+var sensorvalue = {
+	'WATERLEVEL': {
+		inc: 1,
+		add: 10,
+		stu: 3500,
+		maxlength: 4,
+		min: 3500,
+		max: 4000,
+		div: 1
+	},
+	'TEMPERATURE': {
+		inc: 1,
+		add: 10,
+		stu: 270,
+		maxlength: 4,
+		min: 1,
+		max: 500,
+		div: 10
+	},
+	'RH': {
+		inc: 1,
+		add: 10,
+		stu: 50,
+		maxlength: 3,
+		min: 1,
+		max: 100,
+		div: 1
+	},
+	'CO2': {
+		inc: 1,
+		add: 10,
+		stu: 700,
+		maxlength: 4,
+		min: 100,
+		max: 3000,
+		div: 1
+	},
+	'ELECTRONS': {
+		inc: 1,
+		add: 10,
+		stu: 500,
+		maxlength: 4,
+		min: 10,
+		max: 2000,
+		div: 1
+	},
+	'PH': {
+		inc: 1,
+		add: 10,
+		stu: 70,
+		maxlength: 4,
+		min: 10,
+		max: 130,
+		div: 10
+	}
+};
